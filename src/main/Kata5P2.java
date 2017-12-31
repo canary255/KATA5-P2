dfdca74a08a6801ff112cf24769af59d92ea2304
ismael <ismael@192.168.1.34>
2017-12-31 15:17:47 +0000

    KATA5-P2 ver 1.0

--- src/main/Kata5P2.java
+++ src/main/Kata5P2.java
@@ -1,27 +1,51 @@
 package kata5P2.main;
-  
-  
+
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import kata5P2.view.HistogramDisplay;
 import kata5P2.model.Histogram;
 import kata5P2.model.Mail;
 import kata5P2.view.MailHistogramBuilder;
 import kata5P2.view.MailListReader;
-  
+import kata5P2.view.MailListReaderDDBB;
+
 public class Kata5P2 {
-  
+
+    ArrayList<String> arrayMail;
+    private String filename;
+    private List<Mail> mailList;
     private Histogram<String> histogram;
     private HistogramDisplay histoDisplay;
     public static void main(String[] args) throws FileNotFoundException, IOException, Exception {
         Kata5P2 kata5 = new Kata5P2();
         kata5.execute();  
     }
-      
-      private void input() throws FileNotFoundException, IOException{
+
+    public Kata5P2() throws ClassNotFoundException, SQLException {
+        this.arrayMail = MailListReaderDDBB.readDDBB();
+    }
+    
+    private void execute()throws Exception{
+        input();
+        process();
+        output();
+    }
+    
+    private void input() throws FileNotFoundException, IOException, ClassNotFoundException, SQLException{
         filename = "C:/Users/usuario/Documents/NetBeansProjects/kata5/src/kata5P2/main/emails.txt";
-        mailList=MailListReader.read(filename);
+        arrayMail=MailListReaderDDBB.readDDBB();
+    }
+    
+    private void process(){
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+    
+    private void output(){
+        histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute(); 
     }
 }
-    
---
+++ src/view/MailListReaderDDBB.java
@@ -0,0 +1,30 @@
+
+package kata5P2.view;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
+public class MailListReaderDDBB {
+    
+    public static ArrayList<String> readDDBB() throws ClassNotFoundException, SQLException{
+        ArrayList<String> mailList = new ArrayList<>();
+        
+        Class.forName("org.sqlite.JDBC");
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:C:/Users/usuario/Downloads/SQLiteDatabaseBrowserPortable/Data/katas.db");
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT mail FROM MAIL");
+        
+        while(rs.next()){
+            String mail = rs.getString("mail");
+            if(!mail.contains("@"))continue;
+            mailList.add(mail);
+        }
+        return mailList;
+    }
+}
